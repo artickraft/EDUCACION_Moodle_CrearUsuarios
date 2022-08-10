7de87476926c2470cb810dc44e6a8001697c1ef8
Nil Civis <nilcivis@gmail.com>
2022-08-10 16:09:02 +0200

    Pulida interfaz web

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ import pandas as pd
 from unidecode import unidecode
 import streamlit as st
 
+st.set_page_config(
+    page_title="APP Generar Usernames",
+    page_icon="💼",
+)
 
+st.title('📋 Generar *usernames*')
+st.caption('Sube un archivo CSV con las columnas [username,firstname,lastname,email,password,course1,type1] y el programa te devolverá el mismo CSV pero con la columna username rellenada usando la primera palabra del firstname y del lastname unidas por un . y en minúsculas sin caracteres especiales.')
 
 archivo = st.file_uploader("Sube tu CSV", accept_multiple_files=False) # input('Mete tu archivo CSV: ')
 # archivo = archivo.replace('"', '')
@@ -22,3 +28,4 @@ if archivo is not None:
     st.dataframe(df)
     st.download_button(label='Descargar CSV', data=csv, mime='text/csv')
 
+st.caption('Hecho por Nil Civis para © 2022 LETCRAFT DESARROLLOS AUDIOVISUALES, SOCIEDAD LIMITADA. ALL RIGHTS RESERVED')
