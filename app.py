405e0583d639acc30b5ba862dfda87f598b28c0f
Nil Civis <nilcivis@gmail.com>
2022-08-10 15:51:37 +0200

    Incorporado StreamLit

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import pandas as pd
 from unidecode import unidecode
-
-archivo = input('Mete tu archivo CSV: ')
-archivo = archivo.replace('"', '')
-df = pd.read_csv(archivo, sep=';')
-
-nombre_util = df['firstname'].str.lower().str.split(' ').str[0].apply(unidecode)
-
-# df['firstname'] = df['firstname'].str.lower()
-# df['firstname'] = df['firstname'].str.split(' ').str[0].apply(unidecode)
-
-apellido_util = df['lastname'].str.lower().str.split(' ').str[0].apply(unidecode)
-
-# df['lastname'] = df['lastname'].str.lower()
-# df['lastname'] = df['lastname'].str.split(' ').str[0].apply(unidecode)
-
-df['username'] = nombre_util + '.' + apellido_util
+import streamlit as st
 
 
-print(df)
-df.to_csv('Usuarios_Final.csv', index=False, sep=';')
+
+archivo = st.file_uploader("Sube tu CSV", accept_multiple_files=False) # input('Mete tu archivo CSV: ')
+# archivo = archivo.replace('"', '')
+if archivo is not None:
+    df = pd.read_csv(archivo, sep=';')
+
+    nombre_util = df['firstname'].str.lower().str.split(' ').str[0].apply(unidecode)
+    apellido_util = df['lastname'].str.lower().str.split(' ').str[0].apply(unidecode)
+
+    df['username'] = nombre_util + '.' + apellido_util
+
+    @st.cache
+    def convert_df(df):
+        return df.to_csv(index=False, sep=';')
+
+    csv = convert_df(df)
+    st.dataframe(df)
+    st.download_button(label='Descargar CSV', data=csv, mime='text/csv')
+
